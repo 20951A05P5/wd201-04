108344792d897d866c75556c2dd954aa2b83e9dd
20951A05P5 <113233868+20951A05P5@users.noreply.github.com>
2022-11-19 14:29:23 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,31 +1,34 @@
-// todo.js
-const todoList = () => {
-  let all = [];
-  const add = (todoItem) => {
+//todolist
+const todoList =()=>{
+  let all=[];
+  const add=(todoItem)=>{
     all.push(todoItem);
   };
-  const markAsComplete = (index) => {
-    all[index].completed = true;
+  const markAsComplete=(index)=>{
+    all[index].completed=true;
   };
-
-  const overdue = () => {
+  
+//overdue
+  const overdue=()=>{
     return all.filter(
-      (item) => item.dueDate < new Date().toLocaleDateString("en-CA")
+      (item)=>item.dueDate < new Date().toLocaleDateString("en-CA")
     );
   };
-
-  const dueToday = () => {
+  
+//duetoday
+  const dueToday=()=>{
     return all.filter(
-      (item) => item.dueDate === new Date().toLocaleDateString("en-CA")
+      (item=>item.dueDate===new Date().toLocaleDateString("en-CA")
     );
   };
-
-  const dueLater = () => {
+  
+//duelater
+  const dueLater=()=>{
     return all.filter(
-      (item) => item.dueDate > new Date().toLocaleDateString("en-CA")
+      (item)=>item.dueDate > new Date().toLocaleDateString("en-CA")
     );
   };
   return { all, add, markAsComplete, overdue, dueToday, dueLater };
 };
 
-module.exports = todoList;
+module.exports=todoList;
